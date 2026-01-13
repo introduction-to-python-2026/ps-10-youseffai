d3d4b1cf82b2faec1ef194c2bf4382c9eb9a21aa
youseffai <altyoxox@gmail.com>
2026-01-13 17:36:27 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -7,27 +7,29 @@ def load_image(path):
   image = np.array(image)
   return image
 
-def edge_detection(image):
-
+def edge_detection(image_array):
+  # Convert to grayscale
   grayscale_image = np.mean(image_array, axis=2)
 
-
+  # Define kernelY
   kernelY = np.array([
       [1, 2, 1],
       [0, 0, 0],
       [-1, -2, -1]
   ])
 
-
+  # Define kernelX
   kernelX = np.array([
       [-1, 0, 1],
       [-2, 0, 2],
       [-1, 0, 1]
   ])
 
+  # Apply convolution
   edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
   edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
 
+  # Compute edgeMAG
   edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
 
   return edgeMAG
